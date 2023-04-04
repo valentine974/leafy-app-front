84ca7de032d7dff74f32fcff5761ad94ec4977b5
Shengwei LI <51034296+sw-li@users.noreply.github.com>
2023-04-04 20:27:15 +0200

    to be deployed to netlify

--- src/pages/Company/CompanyPage.jsx
+++ src/pages/Company/CompanyPage.jsx
@@ -11,19 +11,31 @@ function CompanyPage() {
   const [siret, setSiret] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const [numberOfVacationDays, setNumberOfVacationDays] = useState("");
+  const [companyId, setCompanyId] = useState("");
+  const [allEmployees, setAllEmployees] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
-    authService.getCompany(id).then((foundCompany) => {
-      const { name, address, siret, imageUrl, numberOfVacationDays } =
+    user && authService.getCompany(id).then((foundCompany) => {
+      const { name, address, siret, imageUrl, numberOfVacationDays, _id } =
         foundCompany.data;
+      setCompanyId(_id);
       setName(name);
       setAddress(address);
       setSiret(siret);
       setImageUrl(imageUrl);
       setNumberOfVacationDays(numberOfVacationDays);
     });
-  }, []);
+  }, [user]);
+
+
+  useEffect(() => {
+    authService.getUsers()
+    .then((foundUsers) => {
+      setAllEmployees(foundUsers.data.filter((user) => user.company === companyId))
+      console.log(allEmployees)
+    });
+  }, [companyId]);
 
   return (
     <div className="pageContainer">
@@ -40,6 +52,23 @@ function CompanyPage() {
           )}
         </>
       )}
+
+      <h1 className="pageTitle"> The Team</h1>
+      {
+        allEmployees && (
+          <>  
+            <div className="cards">
+              {allEmployees.map((employee) => (
+                <div className="card">
+                  <img src={employee.imageUrl} alt="avatar" />
+                  <h3>{employee.name}</h3>
+                  <p>{employee.position}</p>
+                  <p>{employee.email}</p>
+                </div>))}
+            </div>
+          </>
+        )
+      }
     </div>
   );
 }
