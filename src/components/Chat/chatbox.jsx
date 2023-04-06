3ef3e35d3835d595c4f043c95edf2c48da1381be
Shengwei LI <51034296+sw-li@users.noreply.github.com>
2023-04-06 17:16:39 +0200

    added home page feature boxes

---
+++ src/components/Chat/chatbox.jsx
@@ -0,0 +1,34 @@
+
+
+
+function Chatbox() {
+    return (  
+        <>
+            user && (
+                <div className="chatbox">
+                    <div className="chatbox__header">
+                        <div className="chatbox__header__left">
+
+                        </div>
+                        <div className="chatbox__header__right">
+
+                        </div>
+                    </div>
+                    <div className="chatbox__body">
+                        <div className="chatbox__body__left">
+
+                        </div>
+                        <div className="chatbox__body__right">
+
+                        </div>
+                    </div>
+                    <div className="chatbox__footer">
+
+                    </div>
+                </div>
+            )
+        </>
+    );
+}
+
+export default Chatbox;
